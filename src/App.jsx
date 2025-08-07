d0bea1a7f9224c02485babb4a3d033cb8405e2a6
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2025-08-07 22:15:31 +0530

    added dark and light theme using dock icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useTheme } from './context/ThemeContext'; 
+import { FaMoon, FaSun } from 'react-icons/fa';
 import { VscHome, VscArchive, VscAccount, VscSettingsGear } from "react-icons/vsc";
 import Dock from './components/Dock/Docks';
 
 function App() {
+  const { theme, toggleTheme } = useTheme(); // ✅ Fixed
+
   const items = [
     { icon: <VscHome size={18} />, label: 'Home', onClick: () => alert('Home!') },
     { icon: <VscArchive size={18} />, label: 'Archive', onClick: () => alert('Archive!') },
     { icon: <VscAccount size={18} />, label: 'Profile', onClick: () => alert('Profile!') },
     { icon: <VscSettingsGear size={18} />, label: 'Settings', onClick: () => alert('Settings!') },
+    {
+      icon: theme === "light" ? <FaMoon size={18} /> : <FaSun size={18} />,
+      label: 'Toggle Theme',
+      onClick: toggleTheme
+    }
   ];
 
   return (
     <>
-      <Dock
-        items={items}
-        panelHeight={50}
-        baseItemSize={40}
-        magnification={60}
-      />
+    Hello
+        <Dock
+      items={items}
+      panelHeight={55}
+      baseItemSize={40}
+      magnification={60}
+      distance={140}
+    />
     </>
-  )
+
+  );
 }
 
-export default App
+export default App;
