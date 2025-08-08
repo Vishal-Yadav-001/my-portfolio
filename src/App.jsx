442aa9b2ba21fe6a02016d63975a464492757b1d
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2025-08-08 21:38:03 +0530

    added new icons for socials

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useTheme } from './context/ThemeContext'; 
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { useTheme } from './context/ThemeContext';
+import { FaGithub, FaLinkedin, FaInstagram, FaMoon, FaSun } from 'react-icons/fa';
 import { VscHome, VscArchive, VscAccount, VscSettingsGear } from "react-icons/vsc";
 import Dock from './components/Dock/Docks';
+import './App.css';
 
 function App() {
   const { theme, toggleTheme } = useTheme(); // ✅ Fixed
 
   const items = [
-    { icon: <VscHome size={18} />, label: 'Home', onClick: () => alert('Home!') },
-    { icon: <VscArchive size={18} />, label: 'Archive', onClick: () => alert('Archive!') },
-    { icon: <VscAccount size={18} />, label: 'Profile', onClick: () => alert('Profile!') },
-    { icon: <VscSettingsGear size={18} />, label: 'Settings', onClick: () => alert('Settings!') },
+      { icon: <VscHome size={18} />, label: 'Home', onClick: () => alert('Home!') },
+    {
+      icon: <FaGithub size={18} />,
+      label: 'GitHub',
+      onClick: () => window.open('https://github.com/Vishal-Yadav-001', '_blank')
+    },
+    {
+      icon: <FaLinkedin size={18} />,
+      label: 'LinkedIn',
+      onClick: () => window.open('https://www.linkedin.com/in/vishal-yadav-741956222/', '_blank')
+    },
     {
       icon: theme === "light" ? <FaMoon size={18} /> : <FaSun size={18} />,
       label: 'Toggle Theme',
@@ -19,16 +27,16 @@ function App() {
   ];
 
   return (
-    <>
-    Hello
-        <Dock
-      items={items}
-      panelHeight={55}
-      baseItemSize={40}
-      magnification={60}
-      distance={140}
-    />
-    </>
+    <div className='container'>
+      Hello
+      <Dock
+        items={items}
+        panelHeight={55}
+        baseItemSize={40}
+        magnification={60}
+        distance={140}
+      />
+    </div>
 
   );
 }
